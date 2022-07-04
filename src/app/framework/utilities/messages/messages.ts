78f7a90d9ddc1f42ff10ea6763295669ccae58e5
mvm283 <mvm283@gmail.com>
2022-07-04 19:35:36 +0200

    all the config has been done

---
+++ src/app/framework/utilities/messages/messages.ts
@@ -0,0 +1,9 @@
+export const Messages = {
+    beforeDelete: 'آیا از حذف ردیف مطین هستید؟',
+    afterDelete: 'حذف با موفقیت انجام شد.',
+    updateRecord:'بروزرسانی انجام شد.',
+    createRecord:'ثبت انجام شد.',
+    requiredAlert:  'این فیلد اجباری است.',
+
+
+  };
--- src/app/indicator/invoice/pre-invoice/pre-invoice.component.ts
+++ src/app/indicator/invoice/pre-invoice/pre-invoice.component.ts
@@ -1,9 +1,7 @@
-import { SelectionChange } from '@angular/cdk/collections';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { AgGridAngular } from 'ag-grid-angular';
-import { CellClickedEvent, ColDef, GridApi, GridReadyEvent, SelectionChangedEvent } from 'ag-grid-community';
-import { Observable } from 'rxjs';
+import { ColDef, GridApi, GridReadyEvent, SelectionChangedEvent } from 'ag-grid-community';
+import { Messages } from 'src/app/framework/utilities/messages/messages';
 import { AlertService } from '../../../framework/utilities/alert/alert-service.service';
 import { Preinvoice } from '../../models/common/preInvoice.model';
 import { PreInvoiceService } from '../../services/pre-invoice.service';
@@ -16,9 +14,7 @@ import { PreInvoiceService } from '../../services/pre-invoice.service';
 export class PreInvoiceComponent implements OnInit {
 
   formGroup: FormGroup;
-  preInvoiceList: Preinvoice[];
   preInvoice: Preinvoice;
-  requiredAlert: string = 'این فیلد اجباری است.';
 
   loading = false;
 
@@ -27,8 +23,7 @@ export class PreInvoiceComponent implements OnInit {
     private alertService: AlertService) { }
 
   ngOnInit() {
-    this.createForm(); 
-    this.getAll();
+    this.createForm();
   }
 
   onSubmit(formGroup) {
@@ -40,76 +35,76 @@ export class PreInvoiceComponent implements OnInit {
       console.log("form is invalid");
       return;
     }
-    
+
     this.loading = true;
     if (!this.formGroup.controls.id.value) {
-        this.create();
+      this.create();
     } else {
-        this.update();
+      this.update();
     }
   }
 
   private create() {
-    console.log("create");
     this.preInvoiceService.create(new Preinvoice(this.formGroup.value))
-        .subscribe((data) => {
+      .subscribe((data) => {
+        this.rowData.push(data);
+        this.agGrid.applyTransaction({
+          add: [data]
+        })!;
+        this.reset();
+      })
+      .add(() => {
+        this.loading = false
+        alert(Messages.createRecord);
+      });
+  }
+  private update() {
+    var id = this.formGroup.controls.id.value;
+    this.preInvoiceService.update(id, this.formGroup.value)
+      .subscribe((data) => {
+        const pi = this.rowData.findIndex(preinvoice => preinvoice.id === id);
+        if (pi != -1) {
+          this.rowData.splice(pi, 1, data);
+          var rowNode = this.agGrid.getRowNode('' + pi);
+          rowNode?.setData(data);
+          this.reset();
+        }
+      })
+      .add(() => {
+        this.loading = false
+        alert(Messages.updateRecord);
+      });
+  }
 
-          this.rowData.push(data);
-          this.agGrid.applyTransaction({
-            add: [data]
-          })!;
+  public delete() {
+    var id1 = this.formGroup.controls.id.value;
+    var result = confirm(Messages.beforeDelete);
+    if (id1 && result) {
+      const pi = this.rowData.findIndex(preinvoice => preinvoice.id === id1);
+      if (pi != -1) {
+        this.preInvoiceService.delete(id1).subscribe(() => {
+          this.rowData.splice(pi, 1);
+
+          const selectedData = this.agGrid.getSelectedRows();
+          this.agGrid.applyTransaction({ remove: selectedData })!;
 
           this.reset();
-          this.alertService.success('preInvoice added');
         })
-        .add(() => this.loading = false);
-}
-private update() {
-  console.log("update");
-    this.preInvoiceService.update(this.formGroup.controls.id.value, this.formGroup.value)
-        .subscribe((data) => {
-            this.alertService.success('preInvoice updated');
+          .add(() => {
+            this.loading = false
+            alert(Messages.afterDelete);
+          });
+      }
 
-            this.rowData.push(data);
-            const res = this.agGrid.applyTransaction({ update: data })!;
-
-
-        })
-        .add(() => this.loading = false);
-}
-private getAll(){
-    
-  this.preInvoiceService.getAll().subscribe((result) => {
-    this.preInvoiceList = result ? result['data'] : [];
-  });
-}
-public delete(){
-  var id1=this.formGroup.controls.id.value;
-  var result = confirm("آیا از حذف ردیف مطین هستید؟");
-  if(id1 && result){
-
-    //const pi = this.preInvoiceList.findIndex( preinvoice => preinvoice.id === id);
-    const pi= this.rowData.findIndex( preinvoice => preinvoice.id === id1);
-   
-    if (pi != -1){
-      this.preInvoiceService.delete(id1).subscribe(() => {
-        this.rowData.splice(pi, 1);
-
-        const selectedData = this.agGrid.getSelectedRows();
-        this.agGrid.applyTransaction({ remove: selectedData })!;
-
-        this.reset();
-        alert('حذف انجام شد.');
-      });
     }
+  }
+
+  reset() {
+    this.formGroup.reset();
 
   }
-}
-reset(){
-  this.formGroup.reset();
 
-}
-createForm() {
+  createForm() {
     this.formGroup = this.formBuilder.group({
       'id': [null],
       'documentNo': [null, [Validators.required]],
@@ -119,61 +114,65 @@ createForm() {
     });
   }
 
+  //form validation
   get getDocumentNo() {
     return this.formGroup.get('documentNo') as FormControl
-  } 
+  }
 
   getErrorDocumentNo() {
-    return this.formGroup.get('documentNo').hasError('required') ? '*' : '' ;
+    return this.formGroup.get('documentNo').hasError('required') ? '*' : '';
   }
- 
-  //grid
-
- // Each Column Definition results in one Column.
- public columnDefs: ColDef[] = [
-  { field: 'id',hide:true},
-  { field: 'documentNo',headerName: 'شماره درخواست'},
-  { field: 'fileNo',headerName: 'شماره فایل'},
-  { field: 'preOrderUnitValue' ,headerName: 'تعداد'},
-  { field: 'vchDate' ,headerName: 'تاریخ'}
-];
- 
-// DefaultColDef sets props common to all Columns
-public defaultColDef: ColDef = {
-  sortable: true,
-  filter: true,
-  resizable: true,
-  floatingFilter: true,
-
-};
-
-// Data that gets displayed in the grid
-//public rowData$!: Observable<any[]>;
-public rowData!: any[];
-
-// For accessing the Grid's API
-//@ViewChild(AgGridAngular) agGrid!: AgGridAngular;
-private agGrid!: GridApi;
-
-  
- // Example load data from sever
- onGridReady(params: GridReadyEvent) {
-  this.agGrid = params.api;
-  this.preInvoiceService.getAll().subscribe((data)=>{
-    this.rowData=data;
-    console.log(data);
-    this.preInvoiceList= data;  
- });
-}
 
 
-// Example of consuming Grid Event
-onSelectionChanged(event: SelectionChangedEvent) {
-  console.log(event.api.getSelectedRows());
-  let pi=new Preinvoice(event.api.getSelectedRows()[0]);
-  console.log(pi);
-  this.formGroup.patchValue(pi);
-}
+
+  //++++++++++++grid
+
+  // Data that gets displayed in the grid
+  //public rowData$!: Observable<any[]>;
+  public rowData!: any[];
+
+  // For accessing the Grid's API
+  private agGrid!: GridApi;
+  private agColumnApi!: any;
+
+
+  // Each Column Definition results in one Column.
+  public columnDefs: ColDef[] = [
+    { field: 'id', hide: true },
+    { field: 'documentNo', headerName: 'شماره درخواست' },
+    { field: 'fileNo', headerName: 'شماره فایل' },
+    { field: 'preOrderUnitValue', headerName: 'تعداد' },
+    { field: 'vchDate', headerName: 'تاریخ' }
+  ];
+
+  // DefaultColDef sets props common to all Columns
+  public defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+    resizable: true,
+    floatingFilter: true,
+
+  };
+
+  // Example load data from sever
+  onGridReady(params: GridReadyEvent) {
+    this.agGrid = params.api;
+    this.agColumnApi = params.columnApi;
+    this.preInvoiceService.getAll().subscribe((data) => {
+      this.rowData = data;
+    });
+  }
+
+  refresh() {
+    this.preInvoiceService.getAll().subscribe((data) => {
+      this.rowData = data;
+    });
+  }
+
+  onSelectionChanged(event: SelectionChangedEvent) {
+    let pi = new Preinvoice(event.api.getSelectedRows()[0]);
+    this.formGroup.patchValue(pi);
+  }
 
 
 }
