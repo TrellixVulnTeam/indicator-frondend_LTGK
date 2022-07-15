bb3e63681d6fcc45f40c2fd60d328dc4ec0b90e5
mvm283 <mvm283@gmail.com>
2022-07-15 19:00:23 +0200

    work on the branch

--- src/app/framework/utilities/messages/messages.ts
+++
@@ -1,9 +0,0 @@
-export const Messages = {
-    beforeDelete: 'آیا از حذف ردیف مطین هستید؟',
-    afterDelete: 'حذف با موفقیت انجام شد.',
-    updateRecord:'بروزرسانی انجام شد.',
-    createRecord:'ثبت انجام شد.',
-    requiredAlert:  'این فیلد اجباری است.',
-
-
-  };
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  PATH_OF_API : 'http://localhost:9091',
-  
-  datemask : [/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]
-};
